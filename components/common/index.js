5f56ae5d33a53c5d01c1dc39303f5c9ca47ab968
Real-Musafir <alshahadath@gmail.com>
2022-04-21 03:31:13 +0600

    seperate some components

--- components/common/index.js
+++ components/common/index.js
@@ -1,2 +1,5 @@
 export { default as Footer } from "./footer";
 export { default as Navbar } from "./navbar";
+
+export { default as Breadcrumbs } from "./breadcrumbs";
+export { default as Hero } from "./hero";
