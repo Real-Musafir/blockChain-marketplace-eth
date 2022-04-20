90162982b6776561fd34a8e12667203ff023ce5e
Real-Musafir <alshahadath@gmail.com>
2022-04-20 20:08:18 +0600

    init tailwind css

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,17 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
-  return <>Hello World</>;
+  return (
+    <>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div className="shrink-0">
+          <img className="h-12 w-12" src="/vercel.svg" alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
+    </>
+  );
 }
