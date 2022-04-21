53bf4706a44029559c526b85280606485005afec
Real-Musafir <alshahadath@gmail.com>
2022-04-21 22:10:27 +0600

    wrap the layout smartly

--- pages/course.js
+++ pages/course.js
@@ -4,13 +4,15 @@ import { BaseLayout } from "@components/common/layout";
 
 export default function Course() {
   return (
-    <BaseLayout>
+    <>
       <div className="py-4">
         <CoureHero />
       </div>
       <Keypoints />
       <Curriculum />
       <Modal />
-    </BaseLayout>
+    </>
   );
 }
+
+Course.Layout = BaseLayout;
