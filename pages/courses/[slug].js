ef915e4c2e967afc825a6ab156dccd1e436c6ab2
Real-Musafir <alshahadath@gmail.com>
2022-04-23 00:50:49 +0600

    make server side rendering in course detail page

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,10 +1,12 @@
 import { Modal } from "@components/common";
 import { CoureHero, Curriculum, Keypoints } from "@components/course";
 import { BaseLayout } from "@components/common/layout";
+import { getAllCourse } from "@content/courses/fetcher";
 
-export default function Course() {
+export default function Course({ course }) {
   return (
     <>
+      {course.title}
       <div className="py-4">
         <CoureHero />
       </div>
@@ -13,6 +15,29 @@ export default function Course() {
       <Modal />
     </>
   );
+}
+
+export function getStaticPaths() {
+  const { data } = getAllCourse();
+
+  return {
+    paths: data.map((c) => ({
+      params: {
+        slug: c.slug,
+      },
+    })),
+    fallback: false,
+  };
+}
+
+export function getStaticProps({ params }) {
+  const { data } = getAllCourse();
+  const course = data.filter((c) => c.slug === params.slug)[0];
+  return {
+    props: {
+      course,
+    },
+  };
 }
 
 Course.Layout = BaseLayout;
