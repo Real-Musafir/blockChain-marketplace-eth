6c20a878b943571dba642c03248d4157f4ff5517
Real-Musafir <alshahadath@gmail.com>
2022-04-27 20:10:41 +0600

    implement SWR hooks to fetch account

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -39,7 +39,7 @@ export default function Navbar() {
                   Loading...
                 </Button>
               ) : isWeb3Loaded ? (
-                account ? (
+                account.data ? (
                   <Button hoverable={false} className="cursor-default">
                     Hi there
                   </Button>
@@ -61,10 +61,10 @@ export default function Navbar() {
           </div>
         </nav>
       </div>
-      {account && (
+      {account.data && (
         <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
           <div className="text-white bg-indigo-600 rounded-md p-2">
-            {account}
+            {account.data}
           </div>
         </div>
       )}
