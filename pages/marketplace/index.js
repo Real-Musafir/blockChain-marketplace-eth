0a45e7b9b99db6d250ff18b12d43544280ca8067
Real-Musafir <alshahadath@gmail.com>
2022-04-30 02:18:27 +0600

    create alert when connect to wrong network

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -12,10 +12,14 @@ export default function Marketplace({ courses }) {
   return (
     <>
       <div className="py-4">
-        <WalletBar network={network.data} address={account.data} />
-        "Current" {`${network.data}`}
-        "Target" {`${network.target}`}
-        "Is Supported" {`${network.isSupported}`}
+        <WalletBar
+          address={account.data}
+          network={{
+            data: network.data,
+            target: network.target,
+            isSupported: network.isSupported,
+          }}
+        />
         <CourseList courses={courses}>
           {(course) => <CourseCard key={course.id} course={course} />}
         </CourseList>
