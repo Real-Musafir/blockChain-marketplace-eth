8124e5850f57332db5da4f638626652a5025f69e
Real-Musafir <alshahadath@gmail.com>
2022-04-29 23:58:33 +0600

    show the network name Instate of chaingId

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -1,19 +1,31 @@
 import { useEffect } from "react";
 import useSWR, { mutate } from "swr";
 
+const NETWORKS = {
+  1: "Ethererum Main Network",
+  3: "Ropstem Test Network",
+  4: "Rinkeby Test Network",
+  5: "Goerli Test Network",
+  42: "Kovan Test Network",
+  16: "Binance Smart Chain",
+  1337: "Ganache",
+};
+
 export const handler = (web3, provider) => () => {
   //prettier-ignore
   const {mutate, ...rest} = useSWR(() => 
     web3 ? "web3/network" : null,
       async () => {
-        const netId = await web3.eth.getChainId();
-        return netId;
+        const chainId = await web3.eth.getChainId();
+        return NETWORKS[chainId];
       }
   );
 
   useEffect(() => {
     provider &&
-      provider.on("chainChanged", (chainId) => mutate(parseInt(chainId, 16)));
+      provider.on("chainChanged", (chainId) =>
+        mutate(NETWORKS[parseInt(chainId, 16)])
+      );
   }, [web3]);
 
   return {
