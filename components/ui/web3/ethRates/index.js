56375b060c899635b2f3c1a5472458135ba3c0b9
Real-Musafir <alshahadath@gmail.com>
2022-05-18 23:06:06 +0600

    emplement loader in ethRate page

--- components/ui/web3/ethRates/index.js
+++ components/ui/web3/ethRates/index.js
@@ -1,4 +1,4 @@
-import { useEthPrice } from "@components/hooks/useEthPrice";
+import { useEthPrice, COURSE_PRICE } from "@components/hooks/useEthPrice";
 import { Loader } from "@components/ui/common";
 import Image from "next/image";
 
@@ -9,9 +9,7 @@ export default function EthRates() {
       <div className="flex flex-1 items-stretch text-center">
         <div className="p-10 border drop-shadow rounded-md">
           <div className="flex items-center">
-            {true ? (
-              <Loader />
-            ) : (
+            {eth.data ? (
               <>
                 <Image
                   layout="fixed"
@@ -21,6 +19,10 @@ export default function EthRates() {
                 />
                 <span className="text-2xl font-bold">ETH = {eth.data}$</span>
               </>
+            ) : (
+              <div className="w-full flex justify-center">
+                <Loader size="md" />
+              </div>
             )}
           </div>
           <p className="text-xl text-gray-500">Current eth Price</p>
@@ -36,7 +38,7 @@ export default function EthRates() {
               width="35"
               src="/small-eth.webp"
             />
-            <span className="text-2xl font-bold">=15$</span>
+            <span className="text-2xl font-bold">={COURSE_PRICE}$</span>
           </div>
           <p className="text-xl text-gray-500">Price per course</p>
         </div>
