7c257d71a3167a18305d1fbc9d6f48b05a0e1c99
Real-Musafir <alshahadath@gmail.com>
2022-05-25 19:18:37 +0600

    load contract with web3 wraping

--- components/ui/layout/base/index.js
+++ components/ui/layout/base/index.js
@@ -1,22 +1,15 @@
 import { Web3Provider } from "@components/providers";
 import Footer from "@components/ui/common/footer";
 import Navbar from "@components/ui/common/navbar";
-import Script from "next/script";
 
 export default function BaseLayout({ children }) {
   return (
-    <>
-      <Script
-        src="/js/truffle-contract.js"
-        strategy="beforeInteractive" //This means it will load first then Web3Provider load
-      />
-      <Web3Provider>
-        <div className=" max-w-7xl mx-auto px-4">
-          <Navbar />
-          <div className="fit">{children}</div>
-        </div>
-        <Footer />
-      </Web3Provider>
-    </>
+    <Web3Provider>
+      <div className=" max-w-7xl mx-auto px-4">
+        <Navbar />
+        <div className="fit">{children}</div>
+      </div>
+      <Footer />
+    </Web3Provider>
   );
 }
--- utils/loadContract.js
+++ utils/loadContract.js
@@ -1,18 +1,39 @@
-export const loadContract = async (name, provider) => {
+const NETWORK_ID = process.env.NEXT_PUBLIC_NETWORK_ID;
+
+export const loadContract = async (name, web3) => {
   const res = await fetch(`/contracts/${name}.json`);
 
   const Artifact = await res.json();
 
-  const _contract = window.TruffleContract(Artifact);
+  let contract = null;
 
-  _contract.setProvider(provider);
-
-  let deployedContract = null;
   try {
-    deployedContract = await _contract.deployed();
+    contract = new web3.eth.Contract(
+      Artifact.abi,
+      Artifact.networks[NETWORK_ID].address
+    );
   } catch {
     console.log(`Contract ${name} can not be loaded`);
   }
 
-  return deployedContract;
+  return contract;
 };
+
+// export const loadContract = async (name, provider) => {
+//   const res = await fetch(`/contracts/${name}.json`);
+
+//   const Artifact = await res.json();
+
+//   const _contract = window.TruffleContract(Artifact);
+
+//   _contract.setProvider(provider);
+
+//   let deployedContract = null;
+//   try {
+//     deployedContract = await _contract.deployed();
+//   } catch {
+//     console.log(`Contract ${name} can not be loaded`);
+//   }
+
+//   return deployedContract;
+// };
