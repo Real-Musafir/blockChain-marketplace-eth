a9bceb6bd2477187f449e834900176ed20b7193c
Real-Musafir <alshahadath@gmail.com>
2022-05-25 18:21:29 +0600

    load contract in an optimized way

--- components/ui/layout/base/index.js
+++ components/ui/layout/base/index.js
@@ -1,15 +1,22 @@
 import { Web3Provider } from "@components/providers";
 import Footer from "@components/ui/common/footer";
 import Navbar from "@components/ui/common/navbar";
+import Script from "next/script";
 
 export default function BaseLayout({ children }) {
   return (
-    <Web3Provider>
-      <div className=" max-w-7xl mx-auto px-4">
-        <Navbar />
-        <div className="fit">{children}</div>
-      </div>
-      <Footer />
-    </Web3Provider>
+    <>
+      <Script
+        src="/js/truffle-contract.js"
+        strategy="beforeInteractive" //This means it will load first then Web3Provider load
+      />
+      <Web3Provider>
+        <div className=" max-w-7xl mx-auto px-4">
+          <Navbar />
+          <div className="fit">{children}</div>
+        </div>
+        <Footer />
+      </Web3Provider>
+    </>
   );
 }
--- utils/loadContract.js
+++ utils/loadContract.js
@@ -1,11 +1,9 @@
-import contract from "@truffle/contract";
-
 export const loadContract = async (name, provider) => {
   const res = await fetch(`/contracts/${name}.json`);
 
   const Artifact = await res.json();
 
-  const _contract = contract(Artifact);
+  const _contract = window.TruffleContract(Artifact);
 
   _contract.setProvider(provider);
 
