ca8d0446579c1a365555f354795aa0a2260f8741
Real-Musafir <alshahadath@gmail.com>
2022-05-30 19:53:05 +0600

    create owned course function

--- components/providers/web3/hooks/setupHooks.js
+++ components/providers/web3/hooks/setupHooks.js
@@ -1,9 +1,11 @@
 import { handler as createAccountHook } from "./useAccount";
 import { handler as createNetworkHook } from "./useNetwork";
+import { handler as createOwnedCourses } from "./useOwnedCourses";
 
 export const setupHooks = (...deps) => {
   return {
     useAccount: createAccountHook(...deps),
     useNetwork: createNetworkHook(...deps),
+    useOwnedCourses: createOwnedCourses(...deps),
   };
 };
