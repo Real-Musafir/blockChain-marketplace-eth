86ac7790a43b718bb35441e6fda25cdc34f4759f
Real-Musafir <alshahadath@gmail.com>
2022-05-31 22:43:11 +0600

    try to solve error

--- components/providers/web3/hooks/useOwnedCourses.js
+++ components/providers/web3/hooks/useOwnedCourses.js
@@ -7,7 +7,25 @@ export const handler = (web3, contract) => (courses, account) => {
       const ownedCourses = [];
       for (let i = 0; i < courses.length; i++) {
         const course = courses[i];
-        ownedCourses.push(course.id);
+
+        // if (!course.id) {
+        //   continue;
+        // }
+        ownedCourses.push(course.owned);
+
+        // const hexCourseId = web3.utils.asciiToHex(course.id);
+        // const courseHash = web3.utils.soliditySha3(
+        //   { type: "bytes16", value: hexCourseId },
+        //   { type: "address", value: account }
+        // );
+
+        // const ownedCourse = await contract.methods
+        //   .getCourseByHash(courseHash)
+        //   .call(); //when we calling a getter function the use call() it has no gas
+
+        // if (ownedCourse.owned != "0x0000000000000000000000000000000000000000") {
+        //   ownedCourse.push(ownedCourse);
+        // }
       }
       return ownedCourses;
     }
