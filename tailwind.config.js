47b9b8a841af1bc014456169b1a891d88f977a85
Real-Musafir <alshahadath@gmail.com>
2022-06-03 20:30:20 +0600

    course card style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       flex: {
         2: "2 2 0%",
+        3: "3 3 0%",
+        4: "4 4 0%",
       },
       maxWidth: {
         "8xl": "1920px",
