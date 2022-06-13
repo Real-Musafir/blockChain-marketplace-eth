b9c9100f0fd9a9913b05650f71648a6c9c8023d0
Real-Musafir <alshahadath@gmail.com>
2022-06-13 09:43:05 +0600

    managed course card

--- components/ui/course/index.js
+++ components/ui/course/index.js
@@ -1,6 +1,7 @@
 export { default as CourseList } from "./list";
 export { default as CourseCard } from "./card";
-export { default as OwnedCourseCard } from "./card/OwnedCourseCard";
+export { default as OwnedCourseCard } from "./card/Owned";
+export { default as ManagedCourseCard } from "./card/Managed";
 
 export { default as CoureHero } from "./hero";
 export { default as Curriculum } from "./curriculum";
