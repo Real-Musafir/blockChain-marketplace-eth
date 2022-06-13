3f58c0faae0761651ecf31f1f8606e8209e3f025
Real-Musafir <alshahadath@gmail.com>
2022-06-13 10:04:07 +0600

    managed card value

--- components/ui/course/card/Managed.js
+++ components/ui/course/card/Managed.js
@@ -1,6 +1,6 @@
-const Item = ({ title, value }) => {
+const Item = ({ title, value, className }) => {
   return (
-    <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-9 sm:gap-4 sm:px-6">
+    <div className={`${className} px-4 py-5  sm:px-6`}>
       <div className="text-sm font-medium text-gray-500">{title}</div>
       <div className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
         {value}
@@ -12,14 +12,17 @@ const Item = ({ title, value }) => {
 export default function ManagedCourseCard({ children, course }) {
   return (
     <div className="bg-white border shadow overflow-hidden sm:rounded-lg mb-3">
-      <div>
-        <div className="border-t border-gray-200">
-          <dl>
-            <Item title="Course ID" value={course.ownedCourseId} />
-            <Item title="Proof" value={course.proof} />
-            <div className="bg-white px-4 py-5 sm:px-6">{children}</div>
-          </dl>
-        </div>
+      <div className="border-t border-gray-200">
+        {Object.keys(course).map((key, i) => (
+          <Item
+            key={key}
+            className={`${i % 2 ? "bg-gray-50" : "bg-white"}`}
+            // title={key}
+            title={key[0].toUpperCase() + key.slice(1)}
+            value={course[key]}
+          />
+        ))}
+        <div className="bg-white px-4 py-5 sm:px-6">{children}</div>
       </div>
     </div>
   );
