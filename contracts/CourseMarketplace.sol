feecf8aeca04210ea73a0068b3a4f90194e57139
Real-Musafir <alshahadath@gmail.com>
2022-06-13 16:08:29 +0600

    verification input

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -3,123 +3,121 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract CourseMarketplace {
 
-    enum State {
-        Purchased,
-        Activated,
-        Deactivated
+  enum State {
+    Purchased,
+    Activated,
+    Deactivated
+  }
+
+  struct Course {
+    uint id; // 32
+    uint price; // 32
+    bytes32 proof; // 32
+    address owner; // 20
+    State state; // 1
+  }
+
+  // mapping of courseHash to Course data
+  mapping(bytes32 => Course) private ownedCourses;
+
+  // mapping of courseID to courseHash
+  mapping(uint => bytes32) private ownedCourseHash;
+
+  // number of all courses + id of the course
+  uint private totalOwnedCourses;
+
+  address payable private owner;
+
+  constructor() {
+    setContractOwner(msg.sender);
+  }
+
+  /// Course has already a Owner!
+  error CourseHasOwner();
+
+  /// Only owner has an access!
+  error OnlyOwner();
+
+  modifier onlyOwner() {
+    if (msg.sender != getContractOwner()) {
+      revert OnlyOwner();
     }
+    _;
+  }
 
-    struct Course {
-        uint id; //32
-        uint price; //32
-        bytes32 proof; //32
-        address owner; //20
-        State state; //1
-    }
-
-    //maping of courseHash to Course data
-    mapping(bytes32 => Course) private ownedCourses;
-
-    // mapping of courseID to courseHash
-    mapping(uint=>bytes32) private ownedCoursHash;
-
-    // number of all courses + id of the course
-    uint private totalOwnedCourses;
-
-    address payable private owner;
-
-    constructor(){
-       setContractOwner(msg.sender);
-    }
-
-    /// Course has already a Owner
-    error CourseHasOwner();
-
-    /// Only Owner has an access!
-    error OnlyOwner();
-
-    modifier onlyOwner(){
-        if(msg.sender != getContractOwner()){
-            revert OnlyOwner();
-        }
-        _;
-    }
-
-    function purchaseCourse(
-            bytes16 courseId, // 0x00000000000000000000000000003130
+  function purchaseCourse(
+    bytes16 courseId, // 0x00000000000000000000000000003130
     bytes32 proof // 0x0000000000000000000000000000313000000000000000000000000000003130
-            ) 
-            external
-            payable
-            {
-                bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
-                
-                if(hasCourseOwnership(courseHash)){
-                    revert CourseHasOwner();
-                }
-                
-                uint id = totalOwnedCourses++;
-                ownedCoursHash[id] = courseHash;
-                ownedCourses[courseHash] = Course({
-                    id: id,
-                    price: msg.value,
-                    proof: proof,
-                    owner: msg.sender,
-                    state: State.Purchased
-                });
-            }
+  )
+    external
+    payable
+  {
+    bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
 
-    function transferOwnership(address newOwner)
+    if (hasCourseOwnership(courseHash)) {
+      revert CourseHasOwner();
+    }
+
+    uint id = totalOwnedCourses++;
+
+    ownedCourseHash[id] = courseHash;
+    ownedCourses[courseHash] = Course({
+      id: id,
+      price: msg.value,
+      proof: proof,
+      owner: msg.sender,
+      state: State.Purchased
+    });
+  }
+
+  function transferOwnership(address newOwner)
     external
     onlyOwner
-    {
+  {
+    setContractOwner(newOwner);
+  }
 
-        setContractOwner(newOwner);
-    }
-        
-    function getCourseCount()
-        external
-        view
-        returns (uint)
-        {
-            return totalOwnedCourses;
-        }
+  function getCourseCount()
+    external
+    view
+    returns (uint)
+  {
+    return totalOwnedCourses;
+  }
 
-    function getCourseHashAtIndex(uint index)
-        external
-        view
-        returns(bytes32)
-        {
-            return ownedCoursHash[index];
-        }
+  function getCourseHashAtIndex(uint index)
+    external
+    view
+    returns (bytes32)
+  {
+    return ownedCourseHash[index];
+  }
 
-    
-    function getCourseByHash(bytes32 courseHash)
-        external
-        view
-        returns(Course memory)
-        {
-            return ownedCourses[courseHash];
-        }
+  function getCourseByHash(bytes32 courseHash)
+    external
+    view
+    returns (Course memory)
+  {
+    return ownedCourses[courseHash];
+  }
 
-    function getContractOwner()
-       public
-       view
-       returns (address)
-       {
-           return owner;
-       }
-    
+  function getContractOwner()
+    public
+    view
+    returns (address)
+  {
+    return owner;
+  }
 
-    function setContractOwner(address newOwner) private {
-        owner = payable(newOwner);
-    } 
+  function setContractOwner(address newOwner) private {
+    owner = payable(newOwner);
+  }
 
-    function hasCourseOwnership(bytes32 courseHash)
-        private
-        view
-        returns (bool)
-        {
-            return ownedCourses[courseHash].owner == msg.sender;
-        }
+  function hasCourseOwnership(bytes32 courseHash)
+    private
+    view
+    returns (bool)
+  {
+    return ownedCourses[courseHash].owner == msg.sender;
+  }
 }
--- pages/marketplace/courses/managed.js
+++ pages/marketplace/courses/managed.js
@@ -10,8 +10,32 @@ import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
 import { useState } from "react";
 
-export default function ManagedCourses() {
+const VerificationInput = ({ onVerify }) => {
   const [email, setEmail] = useState("");
+
+  return (
+    <div className="flex mr-2 relative rounded-md">
+      <input
+        value={email}
+        onChange={({ target: { value } }) => setEmail(value)}
+        type="text"
+        name="account"
+        id="account"
+        className="w-96 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
+        placeholder="0x2341ab..."
+      />
+      <Button
+        onClick={() => {
+          onVerify(email);
+        }}
+      >
+        Verify
+      </Button>
+    </div>
+  );
+};
+
+export default function ManagedCourses() {
   const [proofedOwnership, setProofedOwnership] = useState({});
   const { web3 } = useWeb3();
   const { account } = useAccount();
@@ -23,14 +47,14 @@ export default function ManagedCourses() {
       { type: "bytes32", value: emailHash },
       { type: "bytes32", value: hash }
     );
-    console.log(proofToCheck);
-    console.log(proof);
 
     proofToCheck === proof
       ? setProofedOwnership({
+          ...proofedOwnership,
           [hash]: true,
         })
       : setProofedOwnership({
+          ...proofedOwnership,
           [hash]: false,
         });
   };
@@ -42,27 +66,14 @@ export default function ManagedCourses() {
       <section className="grid grid-cols-1">
         {managedCourses.data?.map((course) => (
           <ManagedCourseCard key={course.ownedCourseId} course={course}>
-            <div className="flex mr-2 relative rounded-md">
-              <input
-                value={email}
-                onChange={({ target: { value } }) => setEmail(value)}
-                type="text"
-                name="account"
-                id="account"
-                className="w-96 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
-                placeholder="0x2341ab..."
-              />
-              <Button
-                onClick={() => {
-                  verifyCourse(email, {
-                    hash: course.hash,
-                    proof: course.proof,
-                  });
-                }}
-              >
-                Verify
-              </Button>
-            </div>
+            <VerificationInput
+              onVerify={(email) => {
+                verifyCourse(email, {
+                  hash: course.hash,
+                  proof: course.proof,
+                });
+              }}
+            />
             {proofedOwnership[course.hash] && (
               <div className="mt-2">
                 <Message>Verified!</Message>
