e448219571166a5be5458aaefeb00c7f41a89723
Real-Musafir <alshahadath@gmail.com>
2022-06-13 16:22:32 +0600

    disabled manage

--- components/providers/web3/hooks/useManagedCourses.js
+++ components/providers/web3/hooks/useManagedCourses.js
@@ -4,7 +4,9 @@ import useSWR from "swr";
 export const handler = (web3, contract) => (account) => {
   const swrRes = useSWR(
     () =>
-      web3 && contract && account ? `web3/managedCourses/${account}` : null,
+      web3 && contract && account.data && account.isAdmin
+        ? `web3/managedCourses/${account.data}`
+        : null,
     async () => {
       const courses = [];
 
