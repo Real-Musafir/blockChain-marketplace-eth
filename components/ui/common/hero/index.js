7f60f8015db3d554e0d779e768dc8c7d2b8c18c9
Real-Musafir <alshahadath@gmail.com>
2022-06-22 09:48:57 +0600

    change header text

--- components/ui/common/hero/index.js
+++ components/ui/common/hero/index.js
@@ -2,11 +2,11 @@ export default function Hero() {
   return (
     <section className="lg:2/6 text-left my-28">
       <div className="text-6xl font-semibold text-gray-900 leading-none">
-        Grow your career as a developer
+        Grow your career as a programmer and buy your favourite books
       </div>
       <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">
         Learn programming and web development the easy way! Get unlimited access
-        to all of our courses.
+        to all of our books.
       </div>
       <div className="mt-5 sm:mt-8 flex lg:justify-start">
         <div className="rounded-md shadow">
